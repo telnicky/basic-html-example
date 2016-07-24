59a16d58e61c4f662ed7615b46b6333c3d2947b8
Travis Elnicky <travis.elnicky@gmail.com>
2016-07-24 13:13:21 -0600

    first commit

---
+++ space.js
@@ -0,0 +1,10 @@
+// $('.link').on({
+//   mouseover: function() {
+//     console.log('boosh')
+//   $(this).css({
+//     position: 'absolute',
+//     left:(Math.random()*200)+"px",
+//     top:(Math.random() * 200) + "px",
+//   });
+//   }
+// });
